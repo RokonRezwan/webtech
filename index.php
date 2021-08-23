afcba414ce1a9e1b62ecb0d3594e75958b436be3
Rezwanur Rais Rokon <67894792+RokonRezwan@users.noreply.github.com>
2021-08-23 11:09:02 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome Page</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+  <div class="container">
+      <br>
+      <br>
+           <h1 class="text-center">Welcome to Student Management System</h1>
+
+      <br>
+      <br>
+      <br>
+      <br>
+
+           <div class="col-sm-offset-4">
+
+           <a class="btn btn-primary text-center" href="admin/login.php">Login</a>
+           <a class="btn btn-primary text-center" href="admin/registration.php">Sign-Up</a>
+           
+           </div>
+
+
+
+      </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
